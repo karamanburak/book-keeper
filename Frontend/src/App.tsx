0c8e8613fc1d7a5c0de51ca80a8702f7feb73805
Burak Karaman <karaman.buraak@gmail.com>
2024-06-17 17:43:25 +0200

    Wrap App component with Provider from react-redux and add store from app/store

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -2,19 +2,23 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import NewBookModal from "./pages/NewBookModal";
 import Navbar from "./components/Navbar";
+import { Provider } from "react-redux";
+import { store } from "./app/store";
 
 // import './App.css'
 function App() {
   return (
-    <div className=' bg-main-blue min-h-screen text-center'>
-      <BrowserRouter>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/" element={<NewBookModal />} />
+    <div className=' bg-main-blue min-h-screen'>
+      <Provider store={store}>
+        <BrowserRouter>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/" element={<NewBookModal />} />
 
-        </Routes>
-      </BrowserRouter>
+          </Routes>
+        </BrowserRouter>
+      </Provider>
     </div>
   )
 }
