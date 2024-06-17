ec8ac5b1856429a439a7cdc4a9666ca7550b0025
Burak Karaman <karaman.buraak@gmail.com>
2024-06-17 18:34:09 +0200

    Create BookModel

--- Backend/src/models/bookModel.js
+++ Backend/src/models/bookModel.js
@@ -1,1 +1,40 @@
 //* Book Models
+const mongoose = require("mongoose");
+
+const bookPostSchema = new mongoose.Schema(
+    {
+        title: {
+            type: String,
+            trim: true,
+            required: true,
+        },
+        author: {
+            type: String,
+            required: true
+        },
+        ISBN: {
+            type: Number,
+            required: true,
+            unique: true
+        },
+        genre: {
+            type: string,
+        },
+        publicationYear: {
+            type: string,
+        },
+        image: {
+            type: string,
+            require: true,
+            unique: true
+        },
+    },
+    {
+        collection: "bookPosts",
+        timestamps: true,
+    }
+)
+
+module.exports = {
+    BookPost: mongoose.model("bookpost", bookPostSchema)
+}
