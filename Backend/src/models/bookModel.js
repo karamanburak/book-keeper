c1f4aadd9ffdb1cda875ab4531982aafff541455
Burak Karaman <karaman.buraak@gmail.com>
2024-06-30 15:26:26 +0200

    Refactor code; add error messages to required fields

--- Backend/src/models/bookModel.js
+++ Backend/src/models/bookModel.js
@@ -2,41 +2,41 @@
 const mongoose = require("mongoose");
 
 const bookPostSchema = new mongoose.Schema(
-    {
-        title: {
-            type: String,
-            trim: true,
-            required: true,
-            unique: true
-        },
-        author: {
-            type: String,
-            required: true
-        },
-        ISBN: {
-            type: Number,
-            required: true,
-            unique: true
-        },
-        genre: {
-            type: String,
-        },
-        publicationYear: {
-            type: Number,
-            required: true
-        },
-        image: {
-            type: String,
-            required: true,
-            unique: true
-        },
+  {
+    title: {
+      type: String,
+      trim: true,
+      required: [true, "The Book must have title!"],
+      unique: true,
     },
-    {
-        collection: "bookPosts",
-        timestamps: true,
-    }
-)
+    author: {
+      type: String,
+      required: [true, "The Book must have a author!"],
+    },
+    ISBN: {
+      type: Number,
+      required: true,
+      unique: true,
+    },
+    genre: {
+      type: String,
+    },
+    publicationYear: {
+      type: Number,
+      required: true,
+    },
+    image: {
+      type: String,
+      required: [true, "The Book must have a image!"],
+      unique: true,
+    },
+  },
+  {
+    collection: "bookPosts",
+    timestamps: true,
+  }
+);
 
 module.exports = {
-    BookPost: mongoose.model("bookpost", bookPostSchema)
-}
+  BookPost: mongoose.model("bookpost", bookPostSchema),
+};
